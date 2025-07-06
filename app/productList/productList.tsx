38e4bfc3d947132e4e4120083c347e18318bc619
Gantavya <gantavyaoo@gmail.com>
2025-07-06 22:22:02 +0530

    resources borrowed from as-55-2

--- app/productList/productList.tsx
+++ app/productList/productList.tsx
@@ -1,19 +1,72 @@
-import data from 'data.json'
+import products from 'data.json'
+import { useState, useEffect } from 'react';
 import ProductCard from '../components/productCard/productCard'
 interface Product {
-  id: string;
-  title: string;
-  img: string;
-  category: string;
-  price: number;
+    id: string;
+    title: string;
+    img: string;
+    category: string;
+    price: number;
+    about: string;
+    brand: string;
 }
 
-export default function ProductList(){
-    return(
-        <div className='flex gap-6 m-2 p-2 flex-wrap max-w-7xl mx-auto'>
-        {data.map((product:Product)=> {
-            return <ProductCard key={product.id} {...product} />
-        })}
+export default function ProductList() {
+    const [data, updateData] = useState<Product[]>(products)
+    const [keyword, updateKeyword] = useState('')
+    const [sort, updateSort] = useState('')
+
+    function Search(event: React.ChangeEvent<HTMLInputElement>) {
+        const word = event.target.value.toLowerCase()
+        updateKeyword(word)
+    }
+
+    function HandleSort(event: React.ChangeEvent<HTMLSelectElement>) {
+        console.log(event.target.value)
+        const way = event.target.value
+        updateSort(way)
+    }
+
+    useEffect(() => {
+        let up = products.filter((item) =>
+            item.title.toLowerCase().includes(keyword) ||
+            item.about.toLowerCase().includes(keyword) ||
+            item.brand.toLowerCase().includes(keyword) ||
+            item.category.toLowerCase().includes(keyword)
+        )
+
+        switch (sort) {
+            case 'l2h':
+                up.sort((a, b) => a.price - b.price)
+                break
+            case 'h2l':
+                up.sort((a, b) => b.price - a.price)
+                break
+            case 'alpha':
+                up.sort((a, b) => a.title.localeCompare(b.title))
+                break
+        }
+        updateData(up)
+    }, [keyword, sort])
+
+
+
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <div className='flex justify-end mt-4 gap-2 items-center'>
+                <select onChange={HandleSort} id="sort" className='text-lg px-2 p-1 border rounded'>
+                    <option value="">Default</option>
+                    <option value="l2h">Sort by Price (asc)</option>
+                    <option value="h2l">Sort by Price (desc)</option>
+                    <option value="alpha">Sort by Title</option>
+                </select>
+                <input className='border rounded p-1 px-2' placeholder='searchbox' onChange={Search} />
+            </div>
+            <div className='flex gap-6 m-2 p-2 flex-wrap justify-center'>
+                {data.length > 0 ? data.map((product: Product) => {
+                    return <ProductCard key={product.id} {...product} />
+                }) : <div className='text-black'>found nothing try searching something else</div>}
+            </div>
         </div>
     )
 }
