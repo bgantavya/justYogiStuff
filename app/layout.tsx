e9f8d59bc267bdbe7cc4451af975b3a1564b7496
Gantavya Bansal <gantavyaoo@gmail.com>
2026-03-06 07:09:45 +0530

    combined proj

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,8 +64,11 @@ export function NotFound() {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <BrowserRouter>
     <Home />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
